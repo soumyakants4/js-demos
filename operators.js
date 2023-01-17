df947d6ea386116c9da11a02e7ff1c38e46e1d6c
Soumyakant Sahoo <soumyakant.sahoo@192.168.1.3>
2023-01-17 17:44:12 +0530

    second commit

--- operators.js
+++ operators.js
@@ -58,9 +58,9 @@ console.log(!true);
 //Functions in JavaScript
 //follows DRY Principle = Do Not Repeat Yourself
 
-function avg(f, g){
-    h = (f + g)/2;
-    return h ;
+function avg(f, g) {
+    h = (f + g) / 2;
+    return h;
 }
 
 c1 = avg(14, 6);
@@ -68,11 +68,77 @@ c2 = avg(50, 20);
 console.log(c1, c2);
 
 
-function bmi(weight, height){
-    k = weight/(height*height)
+function bmi(weight, height) {
+    k = weight / (height * height)
     return k;
 }
 
 stu1 = bmi(80, 1.88);
 stu2 = bmi(92, 1.80);
 console.log(stu1, stu2);
+
+
+
+function linear(x, y, z) {
+    z = x + y;
+    return z;
+}
+
+v1 = linear(20, 10);
+v2 = linear(10, 20);
+
+console.log(v1, v2);
+
+
+//Conditionals in JavaScript
+var age = 43;
+//Single If Statement
+// if (age < 21) {
+//     console.log(`You are under 21. you're not allowed to drink liquor`);
+//     return age;
+// }
+
+//If-Else Statement
+// if (age < 21) {
+//     console.log(`You are under 21. you're not allowed to drink`);
+//     return age;
+// }
+// else {
+//     console.log(`You are 21 or over. you can drink`);
+// }
+
+
+//Multiple If-Else statements
+
+//If Else Ladder
+if (age < 21) {
+    console.log(`You are under 21. you're not allowed to drink liquor`);
+    return age;
+}
+else if (age > 50){
+    console.log(`You are 50. You should avoid liquor`);
+}
+else{
+    console.log(`You are younger than 21. You cannot drink`);
+}
+
+
+// //
+
+
+function checkAge(age){
+
+    if (age < 21) {
+        console.log(`You are under 21. you're not allowed to drink liquor`);
+        return age;
+    }
+    else if (age > 50){
+        console.log(`You are 50. You should avoid liquor`);
+    }
+    else{
+        console.log(`You are younger than 21. You cannot drink`);
+    }
+
+    checkAge(37);   
+     
+}
